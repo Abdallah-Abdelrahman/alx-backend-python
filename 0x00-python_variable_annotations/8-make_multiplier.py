463ff393827c948cf6d77b2e1d433e1b28e06300
Abdallah-Abdelrahman <abdallah_alkaser@outlook.com>
2024-06-06 20:32:48 +0300

    Debug 0x01-8

--- 0x00-python_variable_annotations/8-make_multiplier.py
+++ 0x00-python_variable_annotations/8-make_multiplier.py
@@ -4,6 +4,6 @@ from typing import Callable
 
 
 def make_multiplier(multiplier: float) -> Callable[[float], float]:
-    def mutliply(n):
+    def mutliply(n: float) -> float:
         return n * multiplier
     return mutliply
