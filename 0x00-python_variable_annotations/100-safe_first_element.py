ea49e2ba66c71b8f025aa76015656abba8de491f
Abdallah-Abdelrahman <abdallah_alkaser@outlook.com>
2024-06-06 21:21:18 +0300

    Debug 0x02-100

--- 0x00-python_variable_annotations/100-safe_first_element.py
+++ 0x00-python_variable_annotations/100-safe_first_element.py
@@ -1,10 +1,9 @@
 #!/usr/bin/env python3
 '''Module defines safe_first_element function'''
-from types import NoneType
 from typing import Any, Sequence, Union
 
 
-def safe_first_element(lst: Sequence[Any]) -> Union[Any, NoneType]:
+def safe_first_element(lst: Sequence[Any]) -> Union[Any, None]:
     '''returns list or None'''
     if lst:
         return lst[0]
