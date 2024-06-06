e71db9770efab609936abb8b05a6ba7fe6fc6b04
Abdallah-Abdelrahman <abdallah_alkaser@outlook.com>
2024-06-06 21:56:03 +0300

    Debug 0x02-101

--- 0x00-python_variable_annotations/101-safely_get_value.py
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -1,13 +1,20 @@
 #!/usr/bin/env python3
-'''Module defines safely_get_value function'''
+'''Module defines safely_get_value function
+
+Attrs:
+    T: generic type
+'''
 from typing import Any, Union, Mapping, TypeVar
+
+
+T = TypeVar('T')
 
 
 def safely_get_value(
         dct: Mapping,
         key: Any,
-        default: Union[TypeVar, None] = None
-        ) -> Union[Any, TypeVar]:
+        default: Union[T, None] = None
+        ) -> Union[Any, T]:
     '''return dict member or default value'''
     if key in dct:
         return dct[key]
