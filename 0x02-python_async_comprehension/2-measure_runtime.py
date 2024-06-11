73ab5db64bb535c5dd31b68ba94b2ff00e752b50
Abdallah-Abdelrahman <abdallah_alkaser@outlook.com>
2024-06-11 18:22:30 +0300

    Execute multiple coroutines in parallel using `asyncio.gather`

---
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+'''Module defines `measure_runtime`'''
+import asyncio
+import time
+
+async_comprehension = __import__('1-async_comprehension').async_comprehension
+
+
+async def measure_runtime():
+    '''execute async_comprehension four times in parallel'''
+    start = time.perf_counter()
+    await asyncio.gather(*(async_comprehension() for _ in range(4)))
+    return time.perf_counter() - start
