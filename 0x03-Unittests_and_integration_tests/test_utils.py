062faaa194237b6a66d454ec3e5af10570dbc83a
Abdallah-Abdelrahman <abdallah_alkaser@outlook.com>
2024-07-01 23:16:41 +0800

    Mock http request

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python3
 '''Module defines `TestAccessNestedMap` class'''
-from typing import Mapping, Sequence, Type
 import unittest
+from unittest.mock import patch, Mock
+from typing import Mapping, Sequence, Type
 from parameterized import parameterized
-from utils import access_nested_map
+from utils import access_nested_map, get_json
 
 
 class TestAccessNestedMap(unittest.TestCase):
@@ -29,3 +30,25 @@ class TestAccessNestedMap(unittest.TestCase):
         '''test key error'''
         with self.assertRaises(exception):
             access_nested_map(nested_map, path)
+
+
+class TestGetJson(unittest.TestCase):
+    '''class definition'''
+    @parameterized.expand([
+        ("http://example.com", {"payload": True}),
+        ("http://holberton.io", {"payload": False})
+    ])
+    def test_get_json(self, url, payload):
+        '''mock get reqeust'''
+        mock_response = Mock()
+        mock_response.json.return_value = payload
+
+        with patch('utils.requests.get', return_value=mock_response) as mock:
+            # Call the function
+            result = get_json(url)
+
+            # Check that requests.get was called once with the test_url
+            mock.assert_called_once_with(url)
+
+            # Check that the result is as expected
+            self.assertEqual(result, payload)
