e2af11aa40fcb31ffc592ce623bf8cb675c38d41
Abdallah-Abdelrahman <abdallah_alkaser@outlook.com>
2024-07-02 02:08:43 +0800

    Implement `test_has_license`

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -62,3 +62,13 @@ class TestGithubOrgClient(unittest.TestCase):
             mock_public_repos_url.assert_called_once()
             mock_get_json.assert_called_once_with(
                     'https://api.github.com/orgs/testorg/repos')
+
+    @parameterized.expand([
+        ({"license": {"key": "my_license"}}, "my_license", True),
+        ({"license": {"key": "other_license"}}, "my_license", False),
+        ({}, "my_license", False)
+    ])
+    def test_has_license(self, repo, license_key, expected_result):
+        '''Test GithubOrgClient.has_license'''
+        result = GithubOrgClient.has_license(repo, license_key)
+        self.assertEqual(result, expected_result)
