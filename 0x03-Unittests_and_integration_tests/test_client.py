9530972b9408819f00aa61515c8ba2e9d242cf59
Abdallah-Abdelrahman <abdallah_alkaser@outlook.com>
2024-07-02 01:50:27 +0800

    Mock property

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 '''Module defines `TestAccessNestedMap` class'''
 import unittest
-from unittest.mock import patch
+from unittest.mock import patch, PropertyMock
 from parameterized import parameterized
 from client import GithubOrgClient
 
@@ -22,3 +22,18 @@ class TestGithubOrgClient(unittest.TestCase):
         self.assertEqual(client.org, expected_result)
         mock_get_json.assert_called_once_with(
                 'https://api.github.com/orgs/{}'.format(org_name))
+
+    @patch.object(GithubOrgClient, 'org', new_callable=PropertyMock)
+    def test_public_repos_url(self, mock_org):
+        '''Test that GithubOrgClient._public_repos_url returns the correct URL
+        '''
+        mock_payload = {
+            'repos_url': 'https://api.github.com/orgs/testorg/repos'
+        }
+        mock_org.return_value = mock_payload
+
+        client = GithubOrgClient('testorg')
+        result = client._public_repos_url
+
+        self.assertEqual(result, mock_payload['repos_url'])
+        mock_org.assert_called_once()
