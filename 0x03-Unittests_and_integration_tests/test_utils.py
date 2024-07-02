a69fbfd515301f6aa514673ffd3c14c19fecb2ea
Abdallah-Abdelrahman <abdallah_alkaser@outlook.com>
2024-07-02 10:11:49 +0800

    Debug 0x11-3

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
 '''Module defines:
-`TestAccessNestedMap`, `TestGetJson`, `TestMemoize`
-classes.
+`TestAccessNestedMap`, `TestGetJson`, `TestMemoize` classes.
 '''
 from parameterized import parameterized
 import requests
@@ -16,9 +15,9 @@ class TestAccessNestedMap(unittest.TestCase):
     Methods: test_access_nested_map, test_access_nested_map_exception,
     '''
     @parameterized.expand([
-        ({"a": 1}, ["a",], 1),
-        ({"a": {"b": 2}}, ["a",], {"b": 2}),
-        ({"a": {"b": 2}}, ["a", "b"], 2)
+        ({'a': 1}, ['a',], 1),
+        ({'a': {'b': 2}}, ['a',], {'b': 2}),
+        ({'a': {'b': 2}}, ['a', 'b'], 2)
         ])
     def test_access_nested_map(self,
                                nested_map: Dict[str, Any],
@@ -28,8 +27,8 @@ class TestAccessNestedMap(unittest.TestCase):
         self.assertEqual(access_nested_map(nested_map, path), expected)
 
     @parameterized.expand([
-        ({}, ["a"], KeyError),
-        ({"a": 1}, ["a", "b"], KeyError),
+        ({}, ['a'], KeyError),
+        ({'a': 1}, ['a', 'b'], KeyError),
     ])
     def test_access_nested_map_exception(
             self,
@@ -42,31 +41,31 @@ class TestAccessNestedMap(unittest.TestCase):
 
 
 class TestGetJson(unittest.TestCase):
-    '''class definition to mock http calls'''
+    '''Test the get_json method
+        Methods:
+            test_get_json - test the get_json method, which returns the
+            payload of a request
+    '''
     @parameterized.expand([
-        ("http://example.com", {"payload": True}),
-        ("http://holberton.io", {"payload": False})
+        ('http://example.com', {'payload': True}),
+        ('http://holberton.io', {'payload': False}),
     ])
-    def test_get_json(self, url: str, payload: Dict[str, bool]) -> None:
-        '''mock get reqeust and ensure method called once
-        Args:
-            url:
-            payload:
+    def test_get_json(self, test_url: str,
+                      test_payload: Dict[str, bool]) -> None:
+        '''test_get_json method
+            to test that utils.get_json returns the expected result.
+            Args:
+                test_url: a url
+                test_payload: a payload
         '''
         mock_response = Mock()
-        mock_response.json.return_value = payload
+        mock_response.json.return_value = test_payload
+        with patch.object(requests, 'get',
+                          return_value=mock_response) as mock_method:
+            test_response = get_json(test_url)
+            self.assertEqual(test_response, test_payload)
 
-        with patch.object(requests,
-                          'get',
-                          return_value=mock_response) as mock:
-            # Call the function
-            result = get_json(url)
-
-            # Check that the result is as expected
-            self.assertEqual(result, payload)
-
-        # Check that requests.get was called once
-        mock.assert_called_once()
+        mock_method.assert_called_once()
 
 
 class TestMemoize(unittest.TestCase):
